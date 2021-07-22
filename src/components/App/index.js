2c14243ec3094d2ca03b78a255a10abed5a0a873
baptaste <babachampbenoit@gmail.com>
2021-07-22 09:17:30 +0200

    Setup + rajout prop-types et semantic-UI

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import reactLogo from './react-logo.svg';
+import '../../styles/index.scss';
+import 'semantic-ui-css/semantic.min.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <img src={reactLogo} alt="react logo" />
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
