853aed108385bf7ffc8f221552ad67c1f2be43c8
baptaste <babachampbenoit@gmail.com>
2021-07-22 14:03:53 +0200

    Ajout d'un lien redirigeant vers le repo cliqué sur github

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
@@ -5,18 +6,20 @@ import PropTypes from 'prop-types';
 const Repo = ({
   id, name, description, html_url, owner,
 }) => (
-  <Card className="repo" key={id}>
-    <Image src={owner.avatar_url} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Meta>
-        <span className="date">{owner.login}</span>
-      </Card.Meta>
-      <Card.Description>
-        {description}
-      </Card.Description>
-    </Card.Content>
-  </Card>
+  <a href={html_url} target="_blank" rel="noreferrer" key={id}>
+    <Card className="repo">
+      <Image src={owner.avatar_url} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>
+          <span className="date">{owner.login}</span>
+        </Card.Meta>
+        <Card.Description>
+          {description}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  </a>
 );
 
 Repo.propTypes = {
