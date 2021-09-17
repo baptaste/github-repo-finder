334ec4a00ea57b49a08ceb7b41b5a3a67679f0cd
baptiste <babachampbenoit@gmail.com>
2021-09-17 16:14:12 +0200

    readme updated, final result preview added, console.log & comments removed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Github Repo Finder
+
+Simple react app that helps you to find any repos presents on github by consuming its API.
+
+Build with react (using hooks).
+
+## Stack
+
+react, react-dom, react-router-dom, semantic-ui, axios
+
+## Preview
+
+![](preview.png)
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,11 +14,10 @@ import Repos from '../Repos';
 import Faq from '../Faq';
 import Loading from '../Loading';
 
-// == Composant
+
 const baseUrl = 'https://api.github.com/search/repositories?q=';
 
 const App = () => {
-  // hooks
   const [searchValues, setSearchValues] = useState('');
   const [baseRepos, setBaseRepos] = useState([]);
   const [totalReposCount, setTotalReposCount] = useState(1028313);
@@ -27,8 +26,6 @@ const App = () => {
   const [isError, setIsError] = useState(false);
   const [requestError, setRequestError] = useState('error');
   const [isLoading, setIsLoading] = useState(false);
-
-  // FUNCTIONS
 
   const toggleError = () => setIsError(!isError);
 
@@ -55,8 +52,6 @@ const App = () => {
       setRequestError(`Request failed with status code ${error.response.status}`);
       throw new Error('Request failed', error);
     }
-    // reset searchValue's state
-    // setSearchValues([]);
     setIsLoading(false);
   };
   // default results on landing
@@ -83,9 +78,8 @@ const App = () => {
   // load more results
   const handleShowMoreClick = async () => {
     setCurrentPage(currentPage + 1);
-    // 1er cas, on a une value stockée dans le state
+
     if (searchValues) {
-      console.log(`1er if : rendu spécifique avec ${searchValues} stockée dans le state`);
       try {
         const response = await axios.get(
           `${baseUrl}${searchValues}&sort=stars&order=desc&page=${currentPage + 1}&per_page=9`,
@@ -102,9 +96,8 @@ const App = () => {
         throw new Error('Request failed', error);
       }
     }
-    // 2nd cas, on a pas de value renseignée, on fait un get avec 'javascript' comme valeur par défaut
+
     if (!searchValues) {
-      console.log('2eme if : rendu par défault avec javascript comme value');
       const response = await axios.get(
         `${baseUrl}javascript&sort=stars&order=desc&page=${currentPage + 1}&per_page=9`,
       );
@@ -150,5 +143,4 @@ const App = () => {
   );
 };
 
-// == Export
 export default App;
